79dc9c31e86afd70807d27873aec566cfcddf4c7
Ewen2015 <wang.enqun@outlook.com>
2024-05-10 17:44:37 +0800

    model soft link doesn't work

--- comfyui_setup.sh
+++ comfyui_setup.sh
@@ -7,8 +7,8 @@ git clone https://github.com/comfyanonymous/ComfyUI.git
 # Assume you have setup the stable-diffusion-webui
 # Put your SD checkpoints (the huge ckpt/safetensors files) in: models/checkpoints
 # Put your VAE in: models/vae
-ln -s stable-diffusion-webui/models/Stable-diffusion/* ComfyUI/models/checkpoints/
-ln -s stable-diffusion-webui/models/VAE-approx/* ComfyUI/models/vae/
+cp stable-diffusion-webui/models/Stable-diffusion/* ComfyUI/models/checkpoints/
+cp stable-diffusion-webui/models/VAE-approx/* ComfyUI/models/vae/
 
 cd ComfyUI
 
@@ -17,8 +17,8 @@ python3 -m venv myenv
 source myenv/bin/activate
 
 # Install python dependencies
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
-pip install -r requirements.txt
+python3 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+python3 -m pip install -r requirements.txt
 
 # Running
 nohup python3 main.py --listen '0.0.0.0' &
