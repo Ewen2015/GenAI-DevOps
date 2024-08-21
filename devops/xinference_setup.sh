473663f851c5b5497085d3b0f085ebfab5a4857b
Ewen Wang <wang.enqun@outlook.com>
2024-08-21 17:32:47 +0800

    Update xinference_setup.sh

--- devops/xinference_setup.sh
+++ devops/xinference_setup.sh
@@ -1,8 +1,18 @@
-# install
-pip install xinference
+# create a work directory
+mkdir Xinference
+cd Xinference
 
+# set up virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# install
+pip install xinference -i https://pypi.tuna.tsinghua.edu.cn/simple
 # upgrade
-pip install -U xinference
+pip install -U xinference -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # set up
 nohup xinference-local --host 0.0.0.0 --port 9998 &
+
+# check log
+tail -n 1 -f nohup.out
