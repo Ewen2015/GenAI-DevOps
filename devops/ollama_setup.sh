15d136c027aaf59fb407bad31b544e0e8ed366c8
Ewen Wang <wang.enqun@outlook.com>
2024-08-23 14:45:45 +0800

    Update and rename ollama_upgrade.sh to ollama_setup.sh

---
+++ devops/ollama_setup.sh
@@ -0,0 +1,8 @@
+## Download or upgrade
+curl -fsSL https://ollama.com/install.sh | sh
+
+## Environment variables
+export OLLAMA_MODELS="/data/ollama/.ollama/models"
+export OLLAMA_HOST=0.0.0.0
+export OLLAMA_NUM_PARALLEL=4
+OLLAMA_MAX_LOADED_MODELS=2
