610bef2858eca97e2ee97c18b52d6d35f49ef458
Ewen Wang <wang.enqun@outlook.com>
2024-08-26 10:12:16 +0800

    Update ollama_setup.sh

--- devops/ollama_setup.sh
+++ devops/ollama_setup.sh
@@ -1,8 +1,6 @@
+# https://github.com/ollama/ollama/blob/main/docs/linux.md
+
 ## Download or upgrade
 curl -fsSL https://ollama.com/install.sh | sh
 
-## Environment variables
-export OLLAMA_MODELS="/data/ollama/.ollama/models"
-export OLLAMA_HOST=0.0.0.0
-export OLLAMA_NUM_PARALLEL=4
-export OLLAMA_MAX_LOADED_MODELS=2
+# Create a service file in /etc/systemd/system/ollama.service
