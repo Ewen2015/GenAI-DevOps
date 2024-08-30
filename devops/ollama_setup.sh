01b7d5d7eb9bfcf398b1ab2be5525ac654d911f9
Ewen Wang <wang.enqun@outlook.com>
2024-08-30 13:39:32 +0800

    Update ollama_setup.sh

--- devops/ollama_setup.sh
+++ devops/ollama_setup.sh
@@ -1,6 +1,10 @@
 # https://github.com/ollama/ollama/blob/main/docs/linux.md
 
-## Download or upgrade
+## Download
+curl -fsSL https://ollama.com/install.sh | sh
+
+## Upgrade
+systemctl stop ollama.service
 curl -fsSL https://ollama.com/install.sh | sh
 
 # Create a service file in /etc/systemd/system/ollama.service
