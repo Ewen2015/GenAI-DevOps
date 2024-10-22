97a05173b4d64d2e235fb69963687bd2d6e8bd0a
Ewen Wang <wang.enqun@outlook.com>
2024-10-22 10:49:07 +0800

    Update dify_upgrade.sh

--- devops/dify_upgrade.sh
+++ devops/dify_upgrade.sh
@@ -3,7 +3,7 @@ git checkout main
 git pull origin main
 
 cd docker
-sed -i 's|\(- "${EXPOSE_NGINX_PORT:-5001}:\${NGINX_PORT:-80}"\)|- "${EXPOSE_NGINX_PORT:-8501}:\${NGINX_PORT:-80}"|' docker-compose.yaml
+sed -i 's|\(- '${EXPOSE_NGINX_PORT:-5001}:\${NGINX_PORT:-80}'\)|- '${EXPOSE_NGINX_PORT:-8501}:\${NGINX_PORT:-80}'|' docker-compose.yaml
 docker compose down
 docker compose up -d
 
